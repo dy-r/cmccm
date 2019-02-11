7fcba7f88c6e3c55646e57bc9f9d128b6d363e75
Yi Dai <daiyibj@cn.ibm.com>
2019-02-11 16:55:07 +0800

    cmccm result

--- R/cmccm.R
+++ R/cmccm.R
@@ -3,9 +3,12 @@
 #
 library(MASS)
 
-#data_model = read.csv('西苑数据-判别分析原始变量.csv',fileEncoding = 'GBK',stringsAsFactors = F)
-model_file_path = './model/cmccm.rda'
-cmccmbuildmodel = function(data_model, model_file_path){
+#
+model_file_path = '/home/opencpu/test2/model/cmccm.rda'
+csv_data_path = '西苑数据-判别分析原始变量.csv'
+cmccmbuildmodel = function(csv_data_path){
+
+  data_model = read.csv(csv_data_path, fileEncoding = 'GBK',stringsAsFactors = F)
 
   # 人群分类
   data_model$event = ifelse(data_model$死亡日期=='',0,1)
@@ -41,47 +44,44 @@ cmccmbuildmodel = function(data_model, model_file_path){
 
   qda.model <- qda( group~ .,data=na.omit(data_lda))
 
-  save(qda.model, file='')
+  save(qda.model, file=model_file_path)
 }
 
-
 cmccmpredfunc = function(input){
-  print('cmccmpredfunc')
-  #library(MASS)
+  #print('cmccmpredfunc')
+  library(MASS)
   New_patient = as.data.frame(lapply(input,as.numeric))#as.data.frame(input)
   New_patient[,"肿瘤原发部位_左半肠"] = New_patient[,"f1"]
-  New_patient[,"BRAF突变型"] = New_patient[,"f2"]
-  New_patient[,"RAS突变型"] = New_patient[,"f3"]
-  New_patient[,"KPS评分"] = sqrt(New_patient[,"f4"])
-  New_patient[,"肝转移"] = New_patient[,"f5"]
-  New_patient[,"肺转移"] = New_patient[,"f6"]
-  New_patient[,"肺"] = New_patient[,"f7"]
-  New_patient[,"肾"] = New_patient[,"f8"]
-  New_patient[,"肝"] = New_patient[,"f9"]
+  New_patient[,"RAS突变型"] = New_patient[,"f2"]
+  New_patient[,"BRAF突变型"] = New_patient[,"f3"]
+  New_patient[,"肝转移"] = New_patient[,"f4"]
+  New_patient[,"肺转移"] = New_patient[,"f5"]
+  New_patient[,"KPS评分"] = sqrt(New_patient[,"f6"])
+  New_patient[,"肝"] = New_patient[,"f7"]
+  New_patient[,"肺"] = New_patient[,"f8"]
+  New_patient[,"肾"] = New_patient[,"f9"]
   New_patient[,"脾"] = New_patient[,"f10"]
   New_patient[,"中医临床症状评分总分值"] = sqrt(New_patient[,"f11"])
   New_patient[,"近3个月主症评分总分"] = sqrt(New_patient[,"f12"])
   New_patient[,"症状妨碍生活评分总分"] = sqrt(New_patient[,"f13"])
-  model_file_path = './model/cmccm.rda'
+
   load(model_file_path)
-  #predmodel.train.new_patient = predict(qda.model,New_patient)
+  predmodel.train.new_patient = predict(qda.model,New_patient)
   #predmodel.train.new_patient
-  New_patient
   #print(predmodel.train.new_patient)
-  #decide = ifelse(predmodel.train.new_patient$class=='优势人群','属于','不属于')
-  #result = paste0('此患者属于优势人群概率为：',round(predmodel.train.new_patient$posterior[,2],2),'; ',decide,'优势人群。')
+  decide = ifelse(predmodel.train.new_patient$class=='优势人群','属于','不属于')
+  result = paste0('此患者属于优势人群概率为：',round(predmodel.train.new_patient$posterior[,2],2),'; ',decide,'优势人群。')
   #print(result)
-  #as.character(result)
+  as.character(result)
 }
 
 testpredict = function(){
-  New_patient = data.frame('f4'=70,'f5'=1,'f6'=0,'f7'=0,"f8"=1,"f9"=1,"f10"=0,
+  New_patient = data.frame('f4'=70,'f5'=1,'f6'=0,'f7'=1,"f8"=0,"f9"=1,"f10"=0,
                            "f11"=24,"f12"=7,"f13"=11,
                            "f1"=0,"f2"=0,"f3"=1)
   print(cmccmpredfunc(New_patient))
-  New_patient = data.frame('f4'=sqrt(70),'f5'=1,'f6'=0,'f7'=0,"f8"=1,"f9"=1,"f10"=0,
+  New_patient = data.frame('f4'=sqrt(70),'f5'=1,'f6'=0,'f7'=1,"f8"=0,"f9"=1,"f10"=0,
                            "f11"=sqrt(24),"f12"=sqrt(7),"f13"=sqrt(11),
                            "f1"=0,"f2"=0,"f3"=1)
   print(cmccmpredfunc(New_patient))
 }
-
